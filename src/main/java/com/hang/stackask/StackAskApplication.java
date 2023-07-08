426f6774d13cb9cb3d474991dd10b6993970b07c
thuhang2510 <tunguyet2510@gmail.com>
2023-07-08 20:08:06 +0700

    design class

---
+++ src/main/java/com/hang/stackask/StackAskApplication.java
@@ -0,0 +1,13 @@
+package com.hang.stackask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackAskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackAskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hang/stackask/StackaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hang.stackask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
