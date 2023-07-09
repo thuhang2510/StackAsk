bacf8ded1a39599065e73bbf2bf2f8e1d90a4e21
thuhang2510 <tunguyet2510@gmail.com>
2023-07-09 20:57:16 +0700

    feat(repository): Create UserRepository class

---
+++ src/main/java/com/hang/stackask/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hang.stackask.repository;
+
+import com.hang.stackask.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
