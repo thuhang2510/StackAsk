9462d86d637ea07db93dc469c6d382c3e91bd1be
thuhang2510 <tunguyet2510@gmail.com>
2023-07-13 23:07:09 +0700

    feat(UserServiceImp): add encode password before save to db

--- src/main/java/com/hang/stackask/service/implement/UserServiceImp.java
+++ src/main/java/com/hang/stackask/service/implement/UserServiceImp.java
@@ -7,6 +7,7 @@ import com.hang.stackask.repository.UserRepository;
 import com.hang.stackask.service.interfaces.IUserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -20,10 +21,14 @@ public class UserServiceImp implements IUserService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserData create(AddUserData data) {
         User userEntity = modelMapper.map(data, User.class);
         userEntity.setCreatedTime(LocalDateTime.now());
+        userEntity.setPassword(passwordEncoder.encode(data.getPassword()));
 
         userEntity = userRepository.save(userEntity);
 
