3d8464ee79c0626d9cb7ab46c3b4e84dbf729b71
thuhang2510 <tunguyet2510@gmail.com>
2023-07-14 20:57:57 +0700

    chore(UserRepository): remove annotation Email not use

--- src/main/java/com/hang/stackask/repository/UserRepository.java
+++ src/main/java/com/hang/stackask/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.hang.stackask.repository;
 
 import com.hang.stackask.entity.User;
-import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
