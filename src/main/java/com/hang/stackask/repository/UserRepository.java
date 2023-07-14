a74535e76d0ce04d31e9b769579f6b7e0061e6db
thuhang2510 <tunguyet2510@gmail.com>
2023-07-14 20:57:23 +0700

    feat(UserRepository): add method getUserByEmailAndPasswordAndEnabledIsTrue

--- src/main/java/com/hang/stackask/repository/UserRepository.java
+++ src/main/java/com/hang/stackask/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.hang.stackask.repository;
 
 import com.hang.stackask.entity.User;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User getUserByEmailAndPasswordAndEnabledIsTrue(String email, String password);
 }
