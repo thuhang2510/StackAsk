a75b2959e52903fe28770a6e080518dd06ec68a3
thuhang2510 <tunguyet2510@gmail.com>
2023-07-18 21:57:42 +0700

    feat(Utility): add method getSiteURL

--- src/main/java/com/hang/stackask/utils/Utility.java
+++ src/main/java/com/hang/stackask/utils/Utility.java
@@ -2,11 +2,18 @@ package com.hang.stackask.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
 
 public class Utility {
     public static String mapToJson(Object obj) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(obj);
+    }
+
+    public static String getSiteURL(HttpServletRequest request){
+        String fullURL = request.getRequestURL().toString();
+        return fullURL.substring(0, StringUtils.ordinalIndexOf(fullURL, "/", 5));
     }
 }
 
