12010652983104042d1d96961e42440ecbbc2b1e
thuhang2510 <tunguyet2510@gmail.com>
2023-07-18 22:03:22 +0700

    feat(EmailUtil): create class EmailUtil

---
+++ src/main/java/com/hang/stackask/utils/EmailUtil.java
@@ -0,0 +1,37 @@
+package com.hang.stackask.utils;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+
+@Component
+public class EmailUtil {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    public String sendEmail(String email, String resetPasswordLink) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        String subject = "Here's the link to reset your password";
+        String content = "Hello, You have requested to reset your password."
+                + "Click the link below to change your password"
+                + "<a href=\"" + resetPasswordLink + "\"> Change password</a>" + "";
+
+        helper.setFrom(username, "StackAsk Support");
+        helper.setTo(email);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        javaMailSender.send(message);
+
+        return "Send mail success";
+    }
+}
