d8dbe2b16db18115643dd17ada7c3557f91cc779
thuhang2510 <tunguyet2510@gmail.com>
2023-07-18 21:56:16 +0700

    feat(IUserService): add abstract method sendMail

--- src/main/java/com/hang/stackask/service/interfaces/IUserService.java
+++ src/main/java/com/hang/stackask/service/interfaces/IUserService.java
@@ -2,8 +2,12 @@ package com.hang.stackask.service.interfaces;
 
 import com.hang.stackask.data.AddUserData;
 import com.hang.stackask.data.UserData;
+import jakarta.mail.MessagingException;
+
+import java.io.UnsupportedEncodingException;
 
 public interface IUserService {
     UserData create(AddUserData data);
     UserData getByEmailAndPassword(String email, String password);
+    String sendMail(String email, String siteURL) throws MessagingException, UnsupportedEncodingException;
 }
