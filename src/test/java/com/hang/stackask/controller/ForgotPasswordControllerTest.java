930d648a073535b52e621f6111060a295f911ba1
thuhang2510 <tunguyet2510@gmail.com>
2023-07-19 20:25:32 +0700

    test(ForgotPasswordControllerTest): add test cases relate to api /forgot_password and /reset_password

--- src/test/java/com/hang/stackask/controller/ForgotPasswordControllerTest.java
+++ src/test/java/com/hang/stackask/controller/ForgotPasswordControllerTest.java
@@ -1,10 +1,14 @@
 package com.hang.stackask.controller;
 
+import com.hang.stackask.exception.PasswordMismatchException;
 import com.hang.stackask.exception.UserNotFoundException;
 import com.hang.stackask.request.ForgotPasswordRequest;
+import com.hang.stackask.request.ResetPasswordRequest;
 import com.hang.stackask.service.interfaces.IUserService;
 import com.hang.stackask.utils.Utility;
+import com.hang.stackask.validator.ForgotPasswordValidator;
 import jakarta.mail.MessagingException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +22,8 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @ExtendWith(SpringExtension.class)
@@ -29,19 +35,25 @@ public class ForgotPasswordControllerTest {
     @MockBean
     private IUserService iUserService;
 
+    @MockBean
+    private ForgotPasswordValidator forgotPasswordValidator;
+
     private ForgotPasswordRequest forgotPasswordRequest;
+    private ResetPasswordRequest resetPasswordRequest;
 
     private final String EMAIL = "hangdamthu4@gmail.com";
-    private final String MESSAGE_SEND_EMAIL_SUCCESS = "send email success";
     private final String SITE_URL = "http://localhost/api/v1";
 
     @BeforeEach
     void init(){
         forgotPasswordRequest = ForgotPasswordRequest.builder().email(EMAIL).build();
+        resetPasswordRequest = ResetPasswordRequest.builder().password("123").retypePassword("123").build();
     }
 
     @Test
     void givenEmailValid_whenForgotPassword_thenSendEmailSuccess() throws Exception {
+        final String MESSAGE_SEND_EMAIL_SUCCESS = "send email success";
+
         given(iUserService.sendMail(EMAIL, SITE_URL)).willReturn(MESSAGE_SEND_EMAIL_SUCCESS);
 
         MvcResult mvcResult = mvc.perform(post("/api/v1/forgot_password")
@@ -51,9 +63,10 @@ public class ForgotPasswordControllerTest {
 
         int status = mvcResult.getResponse().getStatus();
         String content = mvcResult.getResponse().getContentAsString();
+        JSONObject jsonObject = new JSONObject(content);
 
         assertEquals(200, status);
-        assertEquals(MESSAGE_SEND_EMAIL_SUCCESS, content);
+        assertEquals(MESSAGE_SEND_EMAIL_SUCCESS, jsonObject.get("resetPasswordToken"));
     }
 
     @Test
@@ -90,5 +103,69 @@ public class ForgotPasswordControllerTest {
 
         assertEquals(500, status);
         assertEquals(message, content);
+    }
+
+    @Test
+    void givenPasswordNotMatch_whenResetPassword_thenResetPasswordFail() throws Exception {
+        resetPasswordRequest.setRetypePassword("not same");
+
+        String resetPasswordToken = "6gT1A7vfnbhXuU9PaRfePxEy1NKkE9QSt9Vfw9b7Maiel5mbQ59AMPL";
+        String nPassword = resetPasswordRequest.getPassword();
+        String messageException = "Password and retype password not match";
+
+        doThrow(new PasswordMismatchException(messageException)).when(forgotPasswordValidator).checkPasswordsMatch(resetPasswordRequest);
+        given(iUserService.resetPassword(resetPasswordToken, nPassword)).willReturn("update password success");
+
+        MvcResult mvcResult = mvc.perform(post("/api/v1/reset_password?token=" + resetPasswordToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(Utility.mapToJson(resetPasswordRequest)))
+                .andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        String content = mvcResult.getResponse().getContentAsString();
+
+        assertEquals(500, status);
+        assertEquals(messageException, content);
+    }
+
+    @Test
+    void givenPasswordValid_whenResetPassword_thenResetPasswordSuccess() throws Exception {
+        String resetPasswordToken = "6gT1A7vfnbhXuU9PaRfePxEy1NKkE9QSt9Vfw9b7Maiel5mbQ59AMPL";
+        String nPassword = resetPasswordRequest.getPassword();
+
+        doNothing().when(forgotPasswordValidator).checkPasswordsMatch(resetPasswordRequest);
+        given(iUserService.resetPassword(resetPasswordToken, nPassword)).willReturn("update password success");
+
+        MvcResult mvcResult = mvc.perform(post("/api/v1/reset_password?token=" + resetPasswordToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(Utility.mapToJson(resetPasswordRequest)))
+                .andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        String content = mvcResult.getResponse().getContentAsString();
+
+        assertEquals(200, status);
+        assertEquals("update password success", content);
+    }
+
+    @Test
+    void givenThrowsException_whenResetPassword_thenResetPasswordFail() throws Exception {
+        String resetPasswordToken = "6gT1A7vfnbhXuU9PaRfePxEy1NKkE9QSt9Vfw9b7Maiel5mbQ59AMPL";
+        String nPassword = resetPasswordRequest.getPassword();
+        String messageException = "exception";
+
+        doNothing().when(forgotPasswordValidator).checkPasswordsMatch(resetPasswordRequest);
+        given(iUserService.resetPassword(resetPasswordToken, nPassword)).willThrow(new RuntimeException(messageException));
+
+        MvcResult mvcResult = mvc.perform(post("/api/v1/reset_password?token=" + resetPasswordToken)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(Utility.mapToJson(resetPasswordRequest)))
+                .andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        String content = mvcResult.getResponse().getContentAsString();
+
+        assertEquals(500, status);
+        assertEquals(messageException, content);
     }
 }
