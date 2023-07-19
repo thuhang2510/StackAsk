e99d30eebd864a55a5f7adc840d167efa0150ab1
thuhang2510 <tunguyet2510@gmail.com>
2023-07-19 20:15:38 +0700

    feat(ForgotPasswordController): add api /forgot_password and reset_password

--- src/main/java/com/hang/stackask/controller/ForgotPasswordController.java
+++ src/main/java/com/hang/stackask/controller/ForgotPasswordController.java
@@ -1,18 +1,19 @@
 package com.hang.stackask.controller;
 
 import com.hang.stackask.request.ForgotPasswordRequest;
+import com.hang.stackask.request.ResetPasswordRequest;
+import com.hang.stackask.response.SendMailResponse;
+import com.hang.stackask.response.UserResponse;
 import com.hang.stackask.service.interfaces.IUserService;
 import com.hang.stackask.utils.Utility;
+import com.hang.stackask.validator.ForgotPasswordValidator;
 import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 
@@ -22,13 +23,24 @@ public class ForgotPasswordController {
     @Autowired
     private IUserService iUserService;
 
+    @Autowired
+    private ForgotPasswordValidator forgotPasswordValidator;
+
     @PostMapping("/forgot_password")
-    public ResponseEntity<String> forgotPassword(@Valid @RequestBody ForgotPasswordRequest forgotPasswordRequest, HttpServletRequest request) throws MessagingException, UnsupportedEncodingException {
+    public ResponseEntity<SendMailResponse> forgotPassword(@Valid @RequestBody ForgotPasswordRequest forgotPasswordRequest, HttpServletRequest request) throws MessagingException, UnsupportedEncodingException {
         String email = forgotPasswordRequest.getEmail();
         String siteURL = Utility.getSiteURL(request);
 
-        String result = iUserService.sendMail(email, siteURL);
+        String resetPasswordToken = iUserService.sendMail(email, siteURL);
+        SendMailResponse sendMailResponse = SendMailResponse.builder().resetPasswordToken(resetPasswordToken).build();
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return new ResponseEntity<>(sendMailResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/reset_password")
+    public ResponseEntity<String> resetPassword(@RequestParam(name = "token") String token, @RequestBody ResetPasswordRequest resetPasswordRequest){
+        forgotPasswordValidator.checkPasswordsMatch(resetPasswordRequest);
+
+        return new ResponseEntity<>(iUserService.resetPassword(token, resetPasswordRequest.getPassword()), HttpStatus.OK);
     }
 }
