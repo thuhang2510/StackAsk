c6d3888a0388f71cfcb96ec77cb03b593ccb67a0
thuhang2510 <tunguyet2510@gmail.com>
2023-07-19 20:21:21 +0700

    feat(FailedToUpdateUserException): create class FailedToUpdateUserException

---
+++ src/main/java/com/hang/stackask/exception/FailedToUpdateUserException.java
@@ -0,0 +1,7 @@
+package com.hang.stackask.exception;
+
+public class FailedToUpdateUserException extends RuntimeException {
+    public FailedToUpdateUserException(String msg) {
+        super(msg);
+    }
+}
