3ef2500b7b012f5c5f6812c528a1606ea7427d09
thuhang2510 <tunguyet2510@gmail.com>
2023-07-24 21:03:45 +0700

    feat(JwtFilterUtil): change getUserFromJwt to getDataFromJwt

--- src/main/java/com/hang/stackask/utils/JwtFilterUtil.java
+++ src/main/java/com/hang/stackask/utils/JwtFilterUtil.java
@@ -1,7 +1,5 @@
 package com.hang.stackask.utils;
 
-import com.hang.stackask.data.UserData;
-import com.hang.stackask.service.interfaces.IUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +11,16 @@ public class JwtFilterUtil {
     @Autowired
     private JwtUtil jwtUtil;
 
-    @Autowired
-    private IUserService iUserService;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(JwtFilterUtil.class);
 
-    public UserData getUserFromJwt(String bearerToken){
-        UserData result = null;
+    public Long getDataFromJwt(String bearerToken){
+        Long result = null;
 
         try {
             String jwt = getInfoAfterStringBearer(bearerToken);
 
             if (StringUtils.hasText(jwt) && jwtUtil.validateToken(jwt)) {
-                Long userId = jwtUtil.getUserIdFromJWT(jwt);
-                result = iUserService.getById(userId);
+                result = jwtUtil.getUserIdFromJWT(jwt);
             }
         } catch (Exception ex) {
             LOGGER.error("failed", ex);
