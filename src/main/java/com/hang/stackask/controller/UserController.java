527f43fb3f84de966f4883d3d1c3861f92798b1b
thuhang2510 <tunguyet2510@gmail.com>
2023-07-24 21:05:50 +0700

    feat(UserController): add api get all users, get user by uuid

--- src/main/java/com/hang/stackask/controller/UserController.java
+++ src/main/java/com/hang/stackask/controller/UserController.java
@@ -1,12 +1,42 @@
 package com.hang.stackask.controller;
 
+import com.hang.stackask.data.UserData;
+import com.hang.stackask.response.UserResponse;
 import com.hang.stackask.service.interfaces.IUserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.hang.stackask.utils.MapperUtil.mapList;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    @Autowired
     private IUserService iUserService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserResponse>> getAll(){
+        List<UserData> userDatas = iUserService.getAll();
+        List<UserResponse> userResponses = mapList(userDatas, UserResponse.class);
+
+        return new ResponseEntity<>(userResponses, HttpStatus.OK);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<UserResponse> get(@PathVariable(value = "uuid") String uuid){
+        UserData userData = iUserService.getByUuid(uuid);
+        UserResponse userResponse = modelMapper.map(userData, UserResponse.class);
+
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
 }
