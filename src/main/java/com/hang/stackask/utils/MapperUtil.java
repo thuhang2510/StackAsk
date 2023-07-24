a1f614e8b223b29905294d7ce608486fe5d21e6f
thuhang2510 <tunguyet2510@gmail.com>
2023-07-24 21:07:03 +0700

    feat(MapperUtil): add method to convert List to other List

---
+++ src/main/java/com/hang/stackask/utils/MapperUtil.java
@@ -0,0 +1,24 @@
+package com.hang.stackask.utils;
+
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapperUtil {
+    private static ModelMapper modelMapper = new ModelMapper();
+
+
+    private MapperUtil() {
+
+
+    }
+
+    public static <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
+
+        return source
+                .stream()
+                .map(element -> modelMapper.map(element, targetClass))
+                .collect(Collectors.toList());
+    }
+}
