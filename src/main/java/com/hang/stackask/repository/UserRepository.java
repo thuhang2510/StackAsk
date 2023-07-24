fe5b9b8fcd13564ccb62d240caefbc1eb924143f
thuhang2510 <tunguyet2510@gmail.com>
2023-07-24 21:02:13 +0700

    feat(UserRepository): add method get user from db

--- src/main/java/com/hang/stackask/repository/UserRepository.java
+++ src/main/java/com/hang/stackask/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.hang.stackask.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserByEmailAndEnabledIsTrue(String email);
     User getUserByResetPasswordTokenAndEnabledIsTrue(String resetPassword);
+    List<User> getAllByEnabledIsTrue();
+    User getUserByUuidAndEnabledIsTrue(String uuid);
 }
