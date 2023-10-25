d2cbb82e6a4ec21783ffa9a1dff768d469ddc086
thuhang2510 <tunguyet2510@gmail.com>
2023-10-25 19:57:56 +0700

    feat: add exception when replyanswer not found

---
+++ src/main/java/com/hang/stackask/exception/ReplyAnswerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hang.stackask.exception;
+
+public class ReplyAnswerNotFoundException extends RuntimeException {
+    public ReplyAnswerNotFoundException(String msg) {
+        super(msg);
+    }
+}
